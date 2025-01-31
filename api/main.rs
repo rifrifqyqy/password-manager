3c2577c8261961844a63bfe374089913205adc10
rifrifqyqy <rifqyh22@gmail.com>
2025-01-31 17:53:55 +0700

    fix:

--- api/main.rs
+++ api/main.rs
@@ -24,7 +24,7 @@ async fn main() -> std::io::Result<()> {
             .route("/entries", web::get().to(get_entries))
             .route("/entry", web::post().to(create_entry))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:3000")?
     .run()
     .await
 }
