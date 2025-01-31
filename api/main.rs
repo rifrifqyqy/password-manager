c84ed31405f4c44be7668177f12188551aea1794
rifrifqyqy <rifqyh22@gmail.com>
2025-01-31 17:58:34 +0700

    fix:

--- api/main.rs
+++ api/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, HttpResponse, Responder};
 use mongodb::{Client, options::ClientOptions};
 use std::sync::Arc;
 use dotenv::dotenv;
@@ -8,6 +8,10 @@ mod models;
 mod controllers;
 
 use controllers::password_controller::{create_entry, get_entries};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Actix Web on Vercel is running!")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -21,10 +25,11 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(client.clone()))
+            .route("/", web::get().to(index))
             .route("/entries", web::get().to(get_entries))
             .route("/entry", web::post().to(create_entry))
     })
-    .bind("0.0.0.0:3000")?
+    .listen(std::net::TcpListener::bind("127.0.0.1:3000")?)?
     .run()
     .await
 }
